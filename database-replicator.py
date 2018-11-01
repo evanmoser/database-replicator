109ee634c03f3935e1eb04b049cb8d321bf02a65
Evan Moser <evanwmoser@outlook.com>
2018-11-01 12:43:43 -0600

    Remove dependency and readability change

--- database-replicator.py
+++ database-replicator.py
@@ -1,6 +1,6 @@
 # pylint: disable=invalid-name
 """ Database replication utility """
-import os, logging, datetime, argparse, pandas as pd, numpy as np
+import os, logging, datetime, argparse, pandas as pd
 from pandas.io import sql
 from app.Config import Config
 from sqlalchemy import create_engine
@@ -85,6 +85,16 @@ if retroactive:
 if not retroactive:
     logging.info("Performing incremental replication.")
 
+    # determine last modified date based on incremental_field and adjust for any provided offset
+    incremental_threshold = data_destination[config.incremental_field].max()
+    logging.info("Established most recent {0} date/time of {1}.".format(config.incremental_field, incremental_threshold))
+
+    offset = datetime.timedelta(hours=config.offset_hours,minutes=config.offset_minutes)
+    logging.info("Adjusting most recent by an offset of {0} hour(s) and {1} minute(s), per configuration.".format(config.offset_hours, config.offset_minutes))
+    
+    incremental_threshold = str(incremental_threshold - offset)
+    logging.info("Incrementally replicating all records with {0} column updated since {1}".format(config.incremental_field, incremental_threshold))
+
     records_to_insert = set(data_source[config.primary_key]).difference(data_destination[config.primary_key])
     records_to_insert = data_source[data_source[config.primary_key].isin(records_to_insert)]
     records_to_insert.to_sql(config.table, engine_destination, index=False, if_exists='append')
@@ -96,17 +106,7 @@ if not retroactive:
         sql.execute(f"DELETE FROM {config.table} WHERE {config.primary_key} = '{record}'", engine_destination)
         logging.debug("Deleted record {} as part of a DELETE action.".format(record))
     logging.info("Deleted {} records.".format(len(records_to_delete)))
-
-    # determine last modified date based on incremental_field and adjust for any provided offset
-    incremental_threshold = data_destination[config.incremental_field].max()
-    logging.info("Established most recent {0} date/time of {1}.".format(config.incremental_field, incremental_threshold))
-
-    offset = datetime.timedelta(hours=config.offset_hours,minutes=config.offset_minutes)
-    logging.info("Adjusting most recent by an offset of {0} hour(s) and {1} minute(s), per configuration.".format(config.offset_hours, config.offset_minutes))
     
-    incremental_threshold = str(incremental_threshold - offset)
-    logging.info("Incrementally replicating all records with {0} column updated since {1}".format(config.incremental_field, incremental_threshold))
-
     records_to_update = data_source[(data_source[config.incremental_field] > incremental_threshold)]
     for record in records_to_update[config.primary_key]:
         sql.execute(f"DELETE FROM {config.table} WHERE {config.primary_key} = '{record}'", engine_destination)
